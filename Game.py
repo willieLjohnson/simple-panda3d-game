b24ec6aaa49f3f8d2a191bf5835028afbd488bb7
willieLjohnson <liwa.johnson@gmail.com>
2020-05-06 16:23:48 -0400

    Update window properties

--- Game.py
+++ Game.py
@@ -1,8 +1,15 @@
-from direct.showbase.ShowBase import  import ShowBase
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        
+
+        properties = WindowProperties()
+        properties.setSize(1000, 750)
+        self.win.requestProperties(properties)
+
+        self.disableMouse()
+
 game = Game()
 game.run()
