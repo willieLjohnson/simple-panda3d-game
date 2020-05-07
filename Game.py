c186cb63c7dee629d0dbb39724b02de761443c56
willieLjohnson <liwa.johnson@gmail.com>
2020-05-07 09:11:31 -0400

    Add ambient lighting to scene

--- Game.py
+++ Game.py
@@ -1,7 +1,9 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties
-
+from panda3d.core import AmbientLight
+from panda3d.core import Vec4
 from direct.actor.Actor import Actor
+
 
 class Game(ShowBase):
     def __init__(self):
@@ -24,6 +26,11 @@ class Game(ShowBase):
         # Top down view
         self.camera.setPos(0, 0, 32)
         self.camera.setP(-90)
+
+        ambientLight = AmbientLight("ambient light")
+        ambientLight.setColor(Vec4(0.2, 0.2, 0.2, 1))
+        self.ambientLightNodePath = self.render.attachNewNode(ambientLight)
+        self.render.setLight(self.ambientLightNodePath)
 
 game = Game()
 game.run()
