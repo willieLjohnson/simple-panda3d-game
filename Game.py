4a71b7893a07087f519442d6d82996c8292db4d0
willieLjohnson <liwa.johnson@gmail.com>
2020-05-06 20:18:40 -0400

    Add placeholder actor

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties
 
+from direct.actor.Actor import Actor
 
 class Game(ShowBase):
     def __init__(self):
@@ -15,6 +16,9 @@ class Game(ShowBase):
         self.environment = self.loader.loadModel("models/environment")
         self.environment.reparentTo(self.render)
 
+        self.tempActor = Actor("Models/PandaChan/act_p3d_chan", {"walk": "Models/PandaChan/a_p3d_chan_run"})
+        self.tempActor.reparentTo(self.render)
+        self.tempActor.setPos(0, 7, 0)
 
 game = Game()
 game.run()
